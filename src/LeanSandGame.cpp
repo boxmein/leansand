eaeea0468d15e07ceb72baf0386f90621b6d64e8
boxmein <wsdf294@gmail.com>
2015-04-19 02:31:27 +0300

    initial object-oriented leansandgame stuff

---
+++ src/LeanSandGame.cpp
@@ -0,0 +1,31 @@
+#include "leansand.h"
+
+void LeanSandGame::draw(uint32_t* pixmap) {
+
+}
+
+void LeanSandGame::update(unsigned int millis, unsigned int deltaTime) {
+
+}
+
+void LeanSandGame::onKeyDown(SDL_KeyboardEvent* keyboardEvent) {
+  SDL_Keysym key = keyboardEvent.keysym;
+
+  switch (key.scancode) {
+    case SDL_SCANCODE_SPACE:
+      paused = !paused;
+      break;
+    case SDL_SCANCODE_ESCAPE:
+      cout << "escape pressed, exiting game...\n";
+      running = !running;
+      break;
+  }
+}
+
+bool LeanSandGame::isRunning() {
+  return running;
+}
+
+bool LeanSandGame::isPaused() {
+  return paused;
+}
