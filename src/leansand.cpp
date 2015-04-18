0fdb02f97a7f5a031a349f8778e1ee2403f888c9
boxmein <wsdf294@gmail.com>
2015-04-19 00:57:07 +0300

    forward std::cout and std::cerr into files

--- src/leansand.cpp
+++ src/leansand.cpp
@@ -3,6 +3,14 @@
 int main (int argc, char** argv) {
   SDL_Window* window;
 
+  std::ofstream new_cerr("stderr.txt");
+  std::streambuf* old_cerr = cerr.rdbuf();
+  cerr.rdbuf(new_cerr.rdbuf());
+
+  std::ofstream new_cout("stdout.txt");
+  std::streambuf* old_cout = cout.rdbuf();
+  cout.rdbuf(new_cout.rdbuf());
+
   SDL_Init(SDL_INIT_VIDEO);
 
   window = SDL_CreateWindow("leansand v" VERSION,
@@ -16,9 +24,18 @@ int main (int argc, char** argv) {
   }
 
 
+
+
   SDL_Delay(3000);
 
   SDL_DestroyWindow(window);
+
+
+  cout.flush();
+  cerr.flush();
+
+  cout.rdbuf(old_cout);
+  cerr.rdbuf(old_cerr);
 
   SDL_Quit();
   return EXIT_SUCCESS;
