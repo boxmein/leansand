ac6e36c638cc0749f15908bc7fbb22894d850a14
boxmein <wsdf294@gmail.com>
2015-04-19 22:03:26 +0300

    document LuaGlobalAPI.h

--- include/LuaGlobalAPI.h
+++ include/LuaGlobalAPI.h
@@ -1,14 +1,36 @@
 #ifndef LUAAPI_H
 #define LUAAPI_H
 
+#include "LuaAPI.h"
 #include <lua.hpp>
 
+/**
+  \brief Lua state and API management
+  \sa LuaAPI
+
+  This class represents a Lua state with a bunch of loaded extras ("APIs"), as
+  well as the ability to run code inside that Lua state. To setup, simply
+  construct the object and attachAPI() as much as you need. Then, runFile() with
+  a Lua script file.
+*/
 class LuaGlobalAPI {
   public:
+    /**
+      Constructs a new LuaGlobalAPI, with its own lua_State and empty list of
+      APIs to be loaded later.
+    */
     LuaGlobalAPI();
     ~LuaGlobalAPI();
+
+    /**
+      Attaches a new LuaAPI* (a thing that defines a Lua API) to the state.
+    */
     void attachAPI(LuaAPI* api);
     void detachAPI(LuaAPI* api);
+
+    /**
+      Run a file inside this Lua state
+    */
     void runFile(string filename);
 
   private:
