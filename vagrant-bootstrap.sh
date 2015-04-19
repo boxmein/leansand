caa965befcf0196afb95a5f03e2a442135cbb638
boxmein <wsdf294@gmail.com>
2015-04-19 23:36:11 +0300

    Separate build commands onto separate files

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
+
 echo "-- Installing packages... --"
-sudo apt-get install -y build-essential cmake libsdl2 libsdl2-dev libsdl2-image-2.0.0 libsdl2-ttf-2.0.0 git
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y libsdl2-dev
+sudo apt-get install -y libsdl2-image-2.0.0
+sudo apt-get install -y libsdl2-ttf-2.0.0
+sudo apt-get install -y git
+
 echo "-- Installing basic GUI for testing... --"
-sudo apt-get install -y xorg fluxbox
+sudo apt-get install -y xorg
+sudo apt-get install -y fluxbox
+
 echo "-- Installing cmake 3.2 --"
 wget -qO cmake.sh http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-i386.sh
 yes | sudo sh cmake.sh
