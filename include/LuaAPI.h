e5e2c1cd544c94c4c999b48a7d13e19db8a43067
boxmein <wsdf294@gmail.com>
2015-05-23 21:13:48 +0300

    generic Lua API definition

--- include/LuaAPI.h
+++ include/LuaAPI.h
@@ -1,13 +1,35 @@
 #ifndef LUAAPI_H
 #define LUAAPI_H
 
+#include "leansand.h"
+
 /**
   \brief An object representing a bunch of Lua functions / etc together.
   \sa LuaGlobalAPI
 */
 class LuaAPI {
-  /** Name of the API (eg for leansand.ui the name = "ui") **/
-  char* name;
+  public:
+    /** Name of the API (eg for leansand.ui the name = "ui") **/
+    char* name;
+    /** You can put stuff in here, but this doesn't initialize the Lua structure */
+    LuaAPI();
+    ~LuaAPI();
+
+    /**
+      \brief Called when the API is attached onto the LuaGlobalAPI.
+      There will be a lua_newtable() at index table_index in the Lua stack, so
+      populate it with whatever you want and make sure the stack ends up even.
+      After the attach() call, the new table will be attached to leansand, using
+      the name provided above.
+
+      \param L the lua state
+      \param table_index the index in the stack containing a table just for this api
+    */
+    void attach(lua_State* L, int table_index);
+    /**
+      \brief TODO: implement module un-loading
+    */
+    void detach(lua_State* L, int table_index);
 };
 
 #endif
