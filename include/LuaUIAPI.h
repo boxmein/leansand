adcc101e21e76df2967370c281e5f2ba763a4591
boxmein <wsdf294@gmail.com>
2015-05-23 21:14:35 +0300

    LuaUIAPI initial definition

---
+++ include/LuaUIAPI.h
@@ -0,0 +1,20 @@
+#ifndef LUAUIAPI_H
+#define LUAUIAPI_H
+
+/**
+  \brief API to access user interface primitives in the game
+  This is the first-ever Lua API for leansand. It can be used to define user
+  interface objects.
+
+  Right now it doesn't do much - attach() is called when the API is loaded into
+  LuaGlobalAPI and detach() is called otherwise.
+*/
+class LuaUIAPI : public LuaAPI {
+  public:
+    LuaUIAPI();
+    ~LuaUIAPI();
+    void attach(lua_State* L, int table_index);
+    void detach(lua_State* L, int table_index);
+};
+
+#endif
---
+++ src/LuaUIAPI.cpp
@@ -0,0 +1,9 @@
+#include "leansand.h"
+
+LuaUIAPI::LuaUIAPI() {}
+LuaUIAPI::~LuaUIAPI() {}
+void LuaUIAPI::detach(lua_State* L, int table_index) {}
+
+void LuaUIAPI::attach(lua_State* L, int table_index) {
+
+}
